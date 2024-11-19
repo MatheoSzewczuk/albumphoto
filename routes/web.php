880b64e6388bfeffaf2034994ef9fc8247225543
Szewczuk <matheo.szewczuk1@gmail.com>
2024-11-19 10:38:04 +0100

    créa route, controllerphoto, layout

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/', function(){
-//     return view('allAlbum');
-// })
+Route::get("/albums", [Controllerphoto::class, 'albums'])->name('albums');
