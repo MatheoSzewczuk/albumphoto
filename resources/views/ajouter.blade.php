28e06d11a2951cf05a11e139c324330875e2b992
Szewczuk <matheo.szewczuk1@gmail.com>
2024-12-10 10:45:15 +0100

    modif ajouter photo & css

--- resources/views/ajouter.blade.php
+++ resources/views/ajouter.blade.php
@@ -7,8 +7,8 @@
             <input type="text" placeholder="Rechercher par tag/utilisateur">
             </div>
             <a href="">
-                <div class="add-album">
-                <p>Ajouter un album</a></p>
+                <div class="add-photo">
+                <p>Ajouter une photo</a></p>
                 <div class="plus-sign">+</div>
             </div>
         </div>
